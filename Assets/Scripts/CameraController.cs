d3e0a11aa41e51e829bd43928ccd788fe037499a
Alceu Costa <alceufc@gmail.com>
2015-04-23 14:45:46 -0300

    Initial commit.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	public GameObject planet;
+		
+	private Rigidbody playerRb;
+
+	public float heightFromGround;
+	public float distanceToPlayer;
+	public float damp;
+	public float rotationDamp;
+	public float minSpeed;
+	
+	// Use this for initialization
+	void Start () {
+		playerRb = player.GetComponent<Rigidbody>();
+		//lastPlayerPosition = player.transform.position;
+
+		updateCamera ();
+	}
+	
+	// Update is called once per frame
+	//void LateUpdate () {
+	void FixedUpdate () {
+		//float playerSpeed = (player.transform.position - lastPlayerPosition).magnitude;
+
+		if (playerRb.velocity.magnitude > minSpeed) {
+			updateCamera();
+		}
+
+		//lastPlayerPosition = player.transform.position;
+	}
+
+	void updateCamera () {
+		Vector3 speedDirection = playerRb.velocity.normalized;
+		Vector3 cameraVector = (transform.position - planet.transform.position).normalized;
+
+		Vector3 targetPosition = player.transform.position +
+			cameraVector*heightFromGround - speedDirection*distanceToPlayer;
+			transform.LookAt(player.transform, cameraVector);
+
+		float distanceToTarget = (transform.position - targetPosition).magnitude;
+		//float lerpFraction = Mathf.Exp (-distanceToTarget);
+		float lerpFraction = 1/(1 + damp*distanceToTarget);
+
+		transform.position = Vector3.Lerp(transform.position, targetPosition, lerpFraction);
+
+		transform.LookAt(player.transform, cameraVector);
+//		Quaternion oldRotation = transform.rotation;
+//		transform.LookAt(player.transform, cameraVector);
+//		Quaternion targetRotation = transform.rotation;
+//
+//		float angleDiff = Vector3.Angle (oldRotation.eulerAngles, targetRotation.eulerAngles) * Mathf.PI / 180.0f;
+//		transform.rotation = Quaternion.Lerp (oldRotation, targetRotation, Mathf.Cos(angleDiff));
+		//transform.rotation = Quaternion.Lerp (oldRotation, targetRotation, Time.time * rotationDamp);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float thrust;
+	public float playerMass;
+	public float gravity;
+	public float brakeFactor;
+
+	public GameObject planet;
+	public GameObject camera;
+
+	private Rigidbody rb;
+
+	//private Vector3 lastPosition;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		//lastPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 playerVector = (transform.position - planet.transform.position).normalized;
+		Vector3 cameraVector = (camera.transform.position - planet.transform.position).normalized;
+		Vector3 forceNormal = Vector3.Cross (playerVector, cameraVector);
+		Vector3 forceVector = Vector3.Cross (playerVector, forceNormal);
+
+		Debug.DrawRay (transform.position, forceVector*3, Color.green);
+		if (moveVertical < 0.0) {
+			// Brake.
+			Vector3 velocity = rb.velocity.normalized;
+			float velocityMag = velocity.magnitude;
+			if (velocityMag > 0.0){
+				rb.AddForce (-velocity.normalized * velocityMag * velocityMag * brakeFactor);
+			}
+		} else if (moveVertical > 0.0) {
+			// Apply Thurst.
+			rb.AddForce (moveVertical*forceVector*thrust);
+		}
+
+		rb.AddForce (-moveHorizontal*forceNormal*thrust);
+
+		// Gravity.
+		rb.AddForce (-playerVector*playerMass*gravity);
+	}	
+}
